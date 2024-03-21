17b64819f38ab840fd880c3b6d565875d153d86c
HIGHCHOLESTEROL1 <81547396+HIGHCHOLESTEROL1@users.noreply.github.com>
2024-03-21 00:24:08 -0400

    Organization
    Organized each getter code into function so they can be prompted when user looks for specific information

--- scrapper.py
+++ scrapper.py
@@ -2,46 +2,42 @@ import requests
 from bs4 import BeautifulSoup
 from PIL import Image
 
-## html data
-url = input("Enter Patent link: ")
-response = requests.get(url, auth = ('user', 'pass'))
-## parse the html data
-soup = BeautifulSoup(response.text, 'html.parser')
-## text data
-text_data = soup.get_text()
-print()
+def main():
+    ## html data
+    url = input("Enter Patent link: ")
+    response = requests.get(url, auth = ('user', 'pass'), encoding = 'utf-8')
+    ## parse the html data
+    soup = BeautifulSoup(response.text, 'html.parser', encoding = 'utf-8')
+    write(soup)
 
-## print html data into seperate file for examination
-f = open("htmlData.txt", 'w')
-f.write(response.text)
-f.close()
 
+## soup text in external file
+def write(soup):
+    f = open("htmlData.txt", 'w', encoding = 'utf-8')
+    f.write(soup.text)
+    f.close()
 
 ## print the Patent number
-## <meta name="citation_patent_number" content="US:D618677:S1">
-patentNum = soup.find('meta', attrs ={'name': 'citation_patent_number'})
-print("PatentNum : " + patentNum.get('content'))
+def patentNum(soup):
+    patentNum = soup.find('meta', attrs ={'name': 'citation_patent_number'})
+    print("PatentNum : " + patentNum.get('content'))
 
 ## country of orgin
-## <dd itemprop="countryName">United States</dd>
-country = soup.find('dd', attrs = {'itemprop': 'countryName'})
-print("Country of orgin : "  + country.text)
+def countryOrgin(soup):
+    country = soup.find('dd', attrs = {'itemprop': 'countryName'})
+    print("Country of orgin : "  + country.text)
 
-## patent display
-## <meta name="citation_pdf_url" content="https://patentimages.storage.googleapis.com/9d/51/c7/a3227ec100304d/USD618677.pdf">
-## imageLink = soup.find('meta', attrs = {'name' : 'citation_pdf_url'}).get('content')
-## image = Image.open(imageLink)
-## image.show()
 
 ## print all the founders
-founders = soup.find_all('meta', attrs={'name': 'DC.contributor'})
-#<meta name="DC.contributor" content="Bartley K. Andre" scheme="inventor">
-print("Inventors: ")
-for founder in founders:
-    if founder.get('scheme') == "inventor":
-        name = founder.get('content')
-        print(name)
+def founders(soup):
+    founders = soup.find_all('meta', attrs={'name': 'DC.contributor'})
+    #<meta name="DC.contributor" content="Bartley K. Andre" scheme="inventor">
+    print("Inventors: ")
+    for founder in founders:
+        if founder.get('scheme') == "inventor":
+            name = founder.get('content')
+            print(name)
 
-    else:
-        print("Current Company assignee: " + founder.get('content'))
+        else:
+            print("Current Company assignee: " + founder.get('content'))
 
