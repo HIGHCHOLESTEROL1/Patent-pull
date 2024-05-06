34f1129611b6bf6cd1daf0155db901268c86c706
HIGHCHOLESTEROL1 <81547396+HIGHCHOLESTEROL1@users.noreply.github.com>
2024-05-06 18:13:06 -0400

    Started implementation of externam browsing
    Based of user input, open a external browser for code to interact with. Using selenium

--- controller/control.py
+++ controller/control.py
@@ -1,6 +1,28 @@
+from lib2to3.pgen2 import driver
 from mechanize import Browser
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import requests
 
-input_data = input("Enter product name / description: ").replace(" ", "+")
-urlLink = "{}?q=({})&oq={}".format("https://patents.google.com/", input_data, input_data)
-products = Browser(urlLink)
-print(urlLink)
+def getSearchData():
+    input_data = input("Enter product name / description: ").replace(" ", "+")
+    return input_data
+
+def control():
+    print("Q to quit")
+    input_data = ""
+    while (input_data.lower() != "q"):
+        input_data = getSearchData()
+        browser = webdriver.Chrome()
+        browser.get("https://patents.google.com/")
+        elem = browser.find_element(By.NAME, 'q')
+        elem.send_keys(input_data, Keys.ARROW_DOWN)
+
+
+        print("Here is a selection of products to your description: \n")
+    exit()
+
+control()
+
