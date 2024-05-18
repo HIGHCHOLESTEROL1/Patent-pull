b152343816de3ec302666d3506aa8cc9d8699bf6
HIGHCHOLESTEROL1 <81547396+HIGHCHOLESTEROL1@users.noreply.github.com>
2024-05-18 13:26:17 -0400

    Homepage additional features

--- patent-app/src/App.js
+++ patent-app/src/App.js
@@ -1,20 +1,42 @@
 import logo from './patentLogo.svg';
+import meImg from './me.jpg';
 import './App.css';
+import { Children } from 'react';
+
+const me = {
+  name: 'Brian Wang-chen',
+  image : meImg,
+  imageSize : 60,
+};
+
+function MeDisplay(){
+  return(
+    <img className = "Avatar" src = {me.image} alt = "Avatar photo" 
+        style = {{width : me.imageSize, height : me.imageSize}}></img>
+  );
+}
+
+function AboutUsButton(){
+  return(
+    <button>
+        About us
+    </button>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="Logo" />
-        <p>
+        <h2>
           Patent Check
+        </h2>
+        <p>
+          by {me.name}
         </p>
-        <a
-          className="App-link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
+        <MeDisplay/>
+        <a href="https://github.com/HIGHCHOLESTEROL1/Patent-pull" target="_blank">GitHub</a>
       </header>
     </div>
   );
